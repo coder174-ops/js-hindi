8147e4c3d5609cece56587475ec12a4f5e3bce70
Neeraj Kumar Verma <neerajjnv2015@gmail.com>
2024-10-28 17:43:22 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=174;
+let accountEmail="neerajuiet2023@gmail.com";
+var accountPassword="23456";
+accountCity="ballia";
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+ */
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
