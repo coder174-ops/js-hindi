44d03f326c62b7d2f8353e261dbd489cce99ddb6
Neeraj Kumar Verma <neerajjnv2015@gmail.com>
2024-10-28 17:46:46 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3+3);
+
+console.log("Neeraj");
+
+let name="Neeraj";
+let age=21;
+let isLoggedIn=false;
+let state=null;
+
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>True/False
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
