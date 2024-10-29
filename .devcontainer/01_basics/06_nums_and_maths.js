ef454474cd9af4a68added21e68ebbe13ebadc30
Neeraj Kumar Verma <neerajjnv2015@gmail.com>
2024-10-29 19:17:30 +0000

    numAndmath

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,42 @@
+const score=400;
+// console.log(score);
+
+const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber=123.455;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds=1000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//***********MATHS******
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.6));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3,45,6,1,34));
+// console.log(Math.max(34,5,74,2));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
+
+
+
+
+
+
+
+
+
+
