48a65e8321e1a2242afcb7ec0193392fe6f083c5
Neeraj Kumar Verma <neerajjnv2015@gmail.com>
2024-11-10 04:06:51 +0000

    one

---
+++ .devcontainer/05_iteration/two.js
@@ -0,0 +1,19 @@
+//while
+let num=0;
+while (num<=10) {
+    console.log(`my position is ${num}`)//
+    num=num+2;
+}
+//for array
+let myarr=['flash','batman','iron man'];
+let index=0;
+while (index < myarr.length) {
+    console.log(`I am ${myarr[index]}`)//
+    index++;
+}
+
+let score=0
+do {
+    console.log(`my score is ${score}`)
+    score++;
+} while (score<=10);
